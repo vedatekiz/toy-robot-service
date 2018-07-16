59eeeb29081a8a87b9aad41fab49f28c0f457a5b
Ekiz Ali Vedat z003jsxt <vedat.ekiz@siemens.com>
2018-07-16 11:18:08 +0300

    Initial Project Setup

---
+++ src/main/java/com/demo/microservice/toyrobotservice/ToyRobotServiceApplication.java
@@ -0,0 +1,12 @@
+package com.demo.microservice.toyrobotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRobotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyRobotServiceApplication.class, args);
+	}
+}
